ec0d1498149d8d6d448353c5357663e973879b09
Tran Trieu Cuong <trantrieucuong@MacBookPro.lan>
2025-09-16 05:11:43 +0700

    cuongtt22 commit đã fix sử lý bằng cách lấy thêm ngày cuối cùng của tháng. có nghĩa ví dụ tháng 8 có 31 ngày, vào ngày cuối cùng của tháng 8 sẽ đc lấy sang tháng sau, sửa thêm xuất thông tin theo tháng, trc là xuất 1 phát hết luông, bh là xuất theo từng tháng,thêm chức năng tìm kiếm theo mã đơn hàng

--- src/main/java/vn/fs/repository/OrderRepository.java
+++ src/main/java/vn/fs/repository/OrderRepository.java
@@ -18,14 +18,22 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
 	List<Order> findAllByUser_UserIdAndStatus(Long userId, int status);
 
-	@Query("SELECT o FROM Order o WHERE FUNCTION('MONTH', o.orderDate) = :month")
-	List<Order> findByMonth(@Param("month") int month);
-	@Query("SELECT o FROM Order o LEFT JOIN FETCH o.cancellation")
+    @Query("SELECT o FROM Order o WHERE FUNCTION('MONTH', o.orderDate) = :month")
+    List<Order> findByMonth(@Param("month") int month);
+
+    @Query("SELECT o FROM Order o LEFT JOIN FETCH o.cancellation")
 	List<Order> findAllWithCancellation();
+    @Query("SELECT o FROM Order o WHERE CAST(o.orderId AS string) LIKE %:keyword%")
+    List<Order> searchByOrderIdLike(@Param("keyword") String keyword);
+    @Query("SELECT o FROM Order o WHERE str(o.orderId) LIKE %:keyword%")
+    List<Order> findByOrderCodeContaining(@Param("keyword") String keyword);
 
 
 
-	@Query("SELECT SUM(o.amount) FROM Order o WHERE o.status = 2")
+
+
+
+    @Query("SELECT SUM(o.amount) FROM Order o WHERE o.status = 2")
 	    Double findTotalRevenue();
 	    
 	 @Query("SELECT COUNT(o) FROM Order o WHERE o.status = 2")
